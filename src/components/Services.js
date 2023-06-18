3f411724b9a046164ce78eccd2d30fd2f10d437e
RachakondaSivaSaiAbhishek <sivasai7507@gmail.com>
2023-06-18 23:38:34 +0530

    services

--- src/components/Services.js
+++ src/components/Services.js
@@ -24,42 +24,42 @@ const Services = () => {
                        <img className='imagecard' src={shocks} alt=''/>
                        <div className='servicecard'>
                             <h4>Shock Repair</h4>
-                            <p>Details about shock repair </p>
+                            <p>Our garage servicing company specializes in expert shock repair, ensuring smooth and comfortable rides for your vehicle. </p>
                         </div>
                   </div>
                   <div className='servicecard1'>
                        <img className='imagecard' src={oil} alt=''/>
                        <div className='servicecard'>
-                            <h4>Shock Repair</h4>
-                            <p>Details about shock repair </p>
+                            <h4>Oil and lubrication of brakes</h4>
+                            <p>Trust us to provide top-notch oil and brake lubrication services, enhancing your vehicle's performance and safety on the road.</p>
                         </div>
                   </div>
                   <div className='servicecard1'>
                        <img className='imagecard' src={clean} alt=''/>
                        <div className='servicecard'>
-                            <h4>Shock Repair</h4>
-                            <p>Details about shock repair </p>
+                            <h4>Cleaning</h4>
+                            <p> Our comprehensive cleaning services will leave your vehicle spotless, both inside and out, giving it a fresh and polished look.</p>
                         </div>
                   </div>
                   <div className='servicecard1'>
                        <img className='imagecard' src={diagno} alt=''/>
                        <div className='servicecard'>
-                            <h4>Shock Repair</h4>
-                            <p>Details about shock repair </p>
+                            <h4>Inspection and analysis</h4>
+                            <p>With our thorough inspection and analysis services, we identify any underlying issues, providing you with accurate diagnostics for prompt repairs.</p>
                         </div>
                   </div>
                   <div className='servicecard1'>
                        <img className='imagecard' src={tune} alt=''/>
                        <div className='servicecard'>
-                            <h4>Shock Repair</h4>
-                            <p>Details about shock repair </p>
+                            <h4>Tuning performance</h4>
+                            <p>Experience optimal performance with our precision tuning services, fine-tuning your vehicle's engine and systems to maximize efficiency and power.</p>
                         </div>
                   </div>
                   <div className='servicecard1'>
                        <img className='imagecard' src={main} alt=''/>
                        <div className='servicecard'>
-                            <h4>Shock Repair</h4>
-                            <p>Tailwind CSS works by scanning all of your HTML files </p>
+                            <h4>Machinery for heavy loads</h4>
+                            <p>Our garage is equipped with state-of-the-art machinery designed to handle heavy loads, ensuring efficient maintenance and repairs for vehicles.</p>
                         </div>
                   </div>
            
