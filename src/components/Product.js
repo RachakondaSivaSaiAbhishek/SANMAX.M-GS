6abd2f5ee7ecb7b39ec619b3d39276e126fd5701
RachakondaSivaSaiAbhishek <sivasai7507@gmail.com>
2023-07-07 06:59:59 +0530

    final

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,45 +3,25 @@ import './Product.css'
 import topImage from '../assets/images/background.jpg'
 
 const Product = () => {
-    return (
-        <div className='cardContainer'>
-            <div className='card'>
-                <img src={ topImage} alt='' />
-                <p className='cardTitle'>Card One</p>
-                <div className='cardDetails'>
-                   <p className='cardDescription'>This text is styled with some of the text formatting 
-                   properties. The heading uses the 
-                   text-align, text-transform, and color properties. The paragraph is
-                   </p>
-                   <p className='cardNumber'>24</p>
-                </div>   
-            </div>
-            <div className='card'>
-                <img src={ topImage} alt='' />
-                <p className='cardTitle'>Card One</p>
-                <div className='cardDetails'>
-                   <p className='cardDescription'>This  is styled with some of the text formatting 
-                   properties. The heading uses the 
-                   text-align, text-transform, and color properties. The paragraph is
-                   </p>
-                   <p className='cardNumber'>24</p>
-                </div>   
-            </div>
-            <div className='card'>
-                <img src={ topImage} alt='' />
-                <p className='cardTitle'>Card One</p>
-                <div className='cardDetails'>
-                   <p className='cardDescription'>This text is styled with some of the text formatting 
-                   properties. The heading uses the 
-                   text-align, text-transform, and color properties. The paragraph is
-                   </p>
-                   <p className='cardNumber'>24</p>
-                </div>   
-            </div>
+  return (
+    <div className='services'>
+        <div className='topbanner'>
+            <Header/>
+            <h3 className='bannertitle'> Our <span className='spancolor' >Products</span></h3><br/><br/><br/><br/>
+            <p className='bannerdesc'>Delivering a top notch quality services to our customers
+            is our priority</p>
+
+        </div>
+        <div className='servicearea'>
+            
             
 
         </div>
-    )
+
+    </div>
+
+    
+  )
 }
 
 export default Product
